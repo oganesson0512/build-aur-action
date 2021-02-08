f02c2187c19643003fb3deaf4a647d95642ff9fb
Oganesson <59897809+oganesson0512@users.noreply.github.com>
2021-02-08 22:38:01 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 git clone "https://github.com/CoolapkLinux/Coolapk-Linux-PKGBUILD.git"
-cd pkgbuild
+cd Coolapk-Linux-PKGBUILD/pkgbuild
 for pkg in `cat ../need-update`
 do
   cd "${pkg}"
